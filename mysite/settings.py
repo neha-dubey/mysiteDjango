1bef2d338723badfb22314593a6a62f5fd24f1a9
admin <neha24.7@rediffmail.com>
2015-08-05 15:58:45 -0400

    added index template, updated urls and settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp'
+    'myapp',
+    'markdown_deux',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/home/neha/djangoProject/mysite/templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
